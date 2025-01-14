8d717cea5f0b50ad1f56534efe70691ed98f6195
Mahmoudbaky <ma7mouudbaky@gmail.com>
2025-01-15 00:48:21 +0300

    stopped at creating the auth config file ....

--- db/seed.ts
+++ db/seed.ts
@@ -7,7 +7,12 @@ import sampleData from "./sample-data";
 (async () => {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
   console.log("done");
 })();
