5999e1ce01c4bcb406b8ceab0e2418785c6d8b4f
MAHMOUD <ma7mouudbaky@gmail.com>
2025-02-08 11:21:12 +0300

    create paypal order

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -8,6 +8,7 @@ import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
 import { OrderItem } from "@/types";
+import { paypal } from "../paypal";
 
 export const createOrder = async () => {
   try {
@@ -113,3 +114,43 @@ export const getOrderById = async (orderId: string) => {
 
   return convertPrismaToJs(fetchedOrder);
 };
+
+// Create new paypal order
+export async function createPayPalOrder(orderId: string) {
+  try {
+    // Get order from database
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (order) {
+      // Create paypal order
+      const paypalOrder = await paypal.createOrder(Number(order.totalPrice));
+
+      // Update order with paypal order id
+      await prisma.order.update({
+        where: { id: orderId },
+        data: {
+          paymentResult: {
+            id: paypalOrder.id,
+            email_address: "",
+            status: "",
+            pricePaid: 0,
+          },
+        },
+      });
+
+      return {
+        success: true,
+        message: "Item order created successfully",
+        data: paypalOrder.id,
+      };
+    } else {
+      throw new Error("Order not found");
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
