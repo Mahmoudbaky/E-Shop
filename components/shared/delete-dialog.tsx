bb0dcb97a189eeeb062066232f0480e943d007e2
MAHMOUD <ma7mouudbaky@gmail.com>
2025-02-15 16:38:07 +0300

    added deletion dialog

--- components/shared/delete-dialog.tsx
+++ components/shared/delete-dialog.tsx
@@ -1,4 +1,18 @@
 "use client";
+import { useState } from "react";
+import { useTransition } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "../ui/button";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 
 /**
  * DeleteDialog component renders a dialog for confirming the deletion of an item.
@@ -18,7 +32,56 @@ const DeleteDialog = ({
   id: string;
   action: (id: string) => Promise<{ success: boolean; message: string }>;
 }) => {
-  return <div>DeleteDialog</div>;
+  const [open, setOpen] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const { toast } = useToast();
+
+  const handleDeleteClick = () => {
+    startTransition(async () => {
+      const res = await action(id);
+
+      if (!res.success) {
+        toast({
+          variant: "destructive",
+          description: res.message,
+        });
+      } else {
+        setOpen(false);
+        toast({
+          description: res.message,
+        });
+      }
+    });
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button size="sm" variant="destructive" className="ml-2">
+          Delete
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <Button
+            variant="destructive"
+            size="sm"
+            disabled={isPending}
+            onClick={handleDeleteClick}
+          >
+            {isPending ? "Deleting..." : "Delete"}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
 
 export default DeleteDialog;
