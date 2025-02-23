1e754f8234b1ee07db4ae1dda7e2a19cd5c62661
Mahmoudbaky <ma7mouudbaky@gmail.com>
2025-02-24 00:31:44 +0300

    search function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,33 @@
-const SearchPage = () => {
+import { getAllProducts } from "@/lib/actions/products.actions";
+import { PAGE_SIZE } from "@/lib/constants";
+
+const SearchPage = async (props: {
+  searchParams: Promise<{
+    q: string;
+    category: string;
+    price: string;
+    rating: string;
+  }>;
+}) => {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await props.searchParams;
+
+  const products = await getAllProducts({
+    query: q,
+    limit: PAGE_SIZE,
+    page: 1,
+    category,
+    price,
+    rating,
+    sort: "desc",
+  });
+
+  // console.log(products);
+
   return <div>SearchPage</div>;
 };
 
