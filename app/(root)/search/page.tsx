5c8ace64676d836362457fbdcb619ffb28d3025b
Mahmoudbaky <ma7mouudbaky@gmail.com>
2025-02-24 20:58:56 +0300

    finished the search component

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -7,6 +7,70 @@ import {
 import { PAGE_SIZE } from "@/lib/constants";
 import Link from "next/link";
 
+// constants
+const prices = [
+  {
+    name: "$1 to $50",
+    value: "1-50",
+  },
+  {
+    name: "$51 to $100",
+    value: "51-100",
+  },
+  {
+    name: "$101 to $200",
+    value: "101-200",
+  },
+  {
+    name: "$201 to $500",
+    value: "201-500",
+  },
+  {
+    name: "$501 to $1000",
+    value: "501-1000",
+  },
+];
+
+const ratings = [4, 3, 2, 1];
+
+const sortOrders = ["newest", "lowest", "highest", "rating"];
+
+export const generateMetadata = async (props: {
+  searchParams: Promise<{
+    q?: string;
+    category?: string;
+    price?: string;
+    rating?: string;
+  }>;
+}) => {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await props.searchParams;
+
+  const isQuerySet = q && q !== "all" && q.trim() !== "";
+  const isCategorySet =
+    category && category !== "all" && category.trim() !== "";
+  const isPriceSet = price && price !== "all" && price.trim() !== "";
+  const isRatingSet = rating && rating !== "all" && rating.trim() !== "";
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `
+      Search ${isQuerySet ? q : ""} 
+      ${isCategorySet ? `: Category ${category}` : ""}
+      ${isPriceSet ? `: Price ${price}` : ""}
+      ${isRatingSet ? `: Rating ${rating}` : ""}`,
+    };
+  } else {
+    return {
+      title: "Search Products",
+    };
+  }
+};
+
 const SearchPage = async (props: {
   searchParams: Promise<{
     q?: string;
@@ -25,33 +89,6 @@ const SearchPage = async (props: {
     sort = "newest",
     page = "1",
   } = await props.searchParams;
-
-  const prices = [
-    {
-      name: "$1 to $50",
-      value: "1-50",
-    },
-    {
-      name: "$51 to $100",
-      value: "51-100",
-    },
-    {
-      name: "$101 to $200",
-      value: "101-200",
-    },
-    {
-      name: "$201 to $500",
-      value: "201-500",
-    },
-    {
-      name: "$501 to $1000",
-      value: "501-1000",
-    },
-  ];
-
-  const ratings = [4, 3, 2, 1];
-
-  const sortOrders = ["newest", "lowest", "highest", "rating"];
 
   // construct filter url
   const constructFilterUrl = ({
