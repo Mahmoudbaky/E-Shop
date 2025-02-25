bd59c45681763c28774069334167854054a0e78f
Mahmoudbaky <ma7mouudbaky@gmail.com>
2025-02-26 00:56:47 +0300

    trying to make stripe form work

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -37,7 +37,7 @@ const OrderDetailsTable = ({
   isAdmin,
 }: {
   order: Order;
-  stripeClientSecret: string | null;
+  stripeClientSecret: string;
   paypalClientId: string;
   isAdmin: boolean;
 }) => {
