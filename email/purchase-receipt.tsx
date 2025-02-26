609ce29bedfd69b5367a37f4452c65eedccd9b73
MAHMOUD <ma7mouudbaky@gmail.com>
2025-02-26 15:58:51 +0300

    making the Email receipt

---
+++ email/purchase-receipt.tsx
@@ -0,0 +1,21 @@
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+import { Order } from "@/types";
+
+const PurchaseReceiptEmail = ({ order }: { order: Order }) => {
+  return <>PurchaseReceipt</>;
+};
+
+export default PurchaseReceiptEmail;
